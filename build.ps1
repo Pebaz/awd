06fea5c0aa3d5de2e196b5fbf06fbb49979b611b
Pebaz <pebazium@gmail.com>
2020-04-21 17:46:54 +0000

    save

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 $raylib_dir = python -c "import raylib as r, pathlib as p;print(p.Path(r.__file__).parent)"
-pyinstaller --hidden-import=win32gui --add-data "$raylib_dir;raylib" --add-data "data;data" --add-data "energy-ball.png;." main.py -n AWD --workpath "bin/build" --distpath "bin"
+pyinstaller --hidden-import=win32gui --add-data "$raylib_dir;raylib" --add-data "data;data" --add-data "energy-ball.png;." main.py -n AWD --workpath "bin/build" --distpath "bin" --noconsole
